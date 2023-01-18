9bcef56f0a5d814f69a789ea657b67bf1168f912
EMA1970 <114694158+EMA1970@users.noreply.github.com>
2023-01-18 10:05:32 -0800

    New Coding Quiz Style and Index html created and editted, will need to input more for script

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+//initial game Start Section 
+let start = document.querySelector("btn_start");
+
+
+//guide section
+let guide = document.querySelector("quiz_guide")
+let exit = document.querySelector("exit")
+let continue = document.querySelector("continue")
+
+let quiz = document.querySelector("quiz")
